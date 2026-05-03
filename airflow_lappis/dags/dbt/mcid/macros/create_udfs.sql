617b3044ab3803071d74bc84562a478303610985
Gustavo Melo <gustavorsdemelo@gmail.com>
2026-05-03 18:02:31 -0300

    feat: added metadata dbt and integration with cosmos

--- airflow_lappis/dags/dbt/mcid/macros/create_udfs.sql
+++ airflow_lappis/dags/dbt/mcid/macros/create_udfs.sql
@@ -1,5 +1,7 @@
 {% macro create_udfs() %}
-
+-- Lock para evitar problemas de concorrência
+-- na chamada da macro no Cosmos
+select pg_advisory_xact_lock(123456789);
 create schema if not exists {{ target.schema }};
 
     {{ create_f_parse_date_br() }}
